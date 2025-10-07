feea5d67328ec341bd7519df0e5affe6b33ff18a
Aflah Mahdi Yazdi <Diyaz.hal22@gmail.com>
2025-10-07 19:33:08 +0700

    Revisi Tugas

--- fungsi_01okt25/FungsiStaticBanyakPakaiParameter.java
+++ fungsi_01okt25/FungsiStaticBanyakPakaiParameter.java
@@ -4,28 +4,22 @@ public class FungsiStaticBanyakPakaiParameter {
 
     // Fungsi luas persegi
     static int luasPersegi(int sisi){
-        int hasil = sisi * sisi;
-        System.out.println("Luas Persegi = " + hasil + " cm");
-        return hasil;
+        return = sisi * sisi;
     }
 
     // Fungsi luas segitiga
     static double luasSegitiga (int alas, int tinggi){
-        double hasil = 0.5 * alas * tinggi;
-        System.out.println("Luas Segitiga = " + hasil + " cm");
-        return hasil;
+        return = 0.5 * alas * tinggi;
     }
 
     // Fungsi volume kubus
     static int volumeKubus(int panjang, int lebar, int tinggi){
-        int hasil = panjang * lebar * tinggi;
-        System.out.println("Volume Kubus = " + hasil + " cm");
-        return hasil;
+        return = panjang * lebar * tinggi;
     }
 
     public static void main(String[] args) {
-        luasPersegi(10);
-        luasSegitiga(10, 15);
-        volumeKubus(2, 3, 5);
+        System.out.println("Luas Persegi = " + luasPersegi(10) + " cm²");
+        System.out.println("Luas Segitiga = " + luasSegitiga(10, 15) + " cm²");
+        System.out.println("Volume Kubus = " + volumeKubus(2, 3, 5) + " cm³");
     }
 }
