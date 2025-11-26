a67a57371f0828918918e21c56fa2138a37d9a73
Mahdi Yazdi <diyaz.hal22@gmail.com>
2025-11-26 08:09:51 +0700

    Revisi Lagi

--- 02PercabanganJava/TugasNestedNilaiAkhirDenganRentangNilai.java
+++ 02PercabanganJava/TugasNestedNilaiAkhirDenganRentangNilai.java
@@ -1,3 +1,4 @@
+
 import java.util.Scanner;
 
 public class TugasNestedNilaiAkhirDenganRentangNilai {
@@ -8,34 +9,38 @@ public class TugasNestedNilaiAkhirDenganRentangNilai {
         try (Scanner scan = new Scanner(System.in)) {
             System.out.println("Inputkan Nilai Akhir Mahasiswa:");
 
-            // Deklarasi Variabel
             int nilai = scan.nextInt();
             String hasil;
 
-            // Percabangan if else nested
-            if (nilai == 100 && nilai >= 95) {
-                hasil = "Nilai A+";
-            } else if (nilai <= 90 && nilai >= 85) {
-                hasil = "Nilai A";
-            } else if (nilai <= 85 && nilai >= 80) {
-                hasil = "Nilai B+";
-            } else if (nilai <= 80 && nilai >= 75) {
-                hasil = "Nilai B";
-            } else if (nilai <= 75 && nilai >= 65) {
-                hasil = "Nilai C+";
-            } else if (nilai <= 65 && nilai >= 55) {
-                hasil = "Nilai C";
-            } else if (nilai <= 55 && nilai >= 50) {
-                hasil = "Nilai D+";
-            } else if (nilai <= 50 && nilai >= 40) {
-                hasil = "Nilai D";
-            } else if (nilai <= 40 && nilai >= 0) {
-                hasil = "Nilai E";
+            if (nilai >= 0 && nilai <= 100) {
+                if (nilai >= 80) {
+                    if (nilai >= 95) {
+                        hasil = "Nilai A+";
+                    } else if (nilai >= 85) {
+                        hasil = "Nilai A";
+                    } else {
+                        hasil = "Nilai B+";
+                    }
+                } else if (nilai >= 65) {
+                    if (nilai >= 75) {
+                        hasil = "Nilai B";
+                    } else if (nilai >= 70) {
+                        hasil = "Nilai C+";
+                    } else {
+                        hasil = "Nilai C";
+                    }
+                } else {
+                    if (nilai >= 50) {
+                        hasil = "Nilai D";
+                    } else {
+                        hasil = "Nilai E";
+                    }
+                }
+
             } else {
-                hasil = "Anda Salah Input Nilai!!";
+                hasil = "Anda Salah Input Nilai!!!";
             }
 
-            // Cetak nilai hasil
             System.out.println(hasil);
         }
     }
